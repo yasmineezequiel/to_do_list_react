f74ed062f065fc06ee1d7584bdb58e47126597c4
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-10-24 19:42:32 +0200

    Adds Header and TodoInput components.

---
+++ my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header'
+import TodoInput from './components/todoinput'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className= "todo-wrapper">
+          <Header />
+          <TodoInput />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
