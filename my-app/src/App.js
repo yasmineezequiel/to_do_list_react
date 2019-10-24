2dac77137c53e4f2ee231b22acda34cda41f36cb
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-10-24 19:01:59 +0200

    Adds gitignore, cleans app.js file and deletes unnecessary folders.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-    
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>To_Do List</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
